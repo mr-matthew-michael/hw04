7e4091eae5513ea902136589f440dc7787e309fd
mr-matthew-michael <mcollin5@unca.edu>
2023-02-22 14:57:04 -0500

    updates

--- js/main.js
+++ js/main.js
@@ -26,6 +26,8 @@ const initPage = async () => {
     showStories(token);
     showPosts(token);
     showProfile(token);
+    showRightPanel(token);
+    showStoriesPanel(token);
 }
 
 initPage();
@@ -54,6 +56,90 @@ const showProfile = token => {
         const profileBio = document.createElement('p');
         profileBio.textContent = data.bio;
         profileContainer.appendChild(profileBio);
+    });
+}
+
+const showRightPanel = token => {
+    fetch("https://photo-app-secured.herokuapp.com/api/suggestions/", {
+        method: "GET",
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + token
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        const profileContainer = document.querySelector('#followers');
+
+        // Clear any existing suggestions in the container
+       profileContainer.innerHTML = '';
+
+        // Loop through each suggested user and create an HTML representation for them
+        data.forEach(user => {
+            const userContainer = document.createElement('div');
+            userContainer.id = 'sugg-followers';
+
+            const profileImage = document.createElement('img');
+            profileImage.src = user.image_url;
+            profileImage.alt = 'Profile Image';
+            userContainer.appendChild(profileImage);
+
+            const textContainer = document.createElement('div');
+            textContainer.id = 'text';
+
+            const profileName = document.createElement('h1');
+            profileName.textContent = user.username;
+            textContainer.appendChild(profileName);
+
+            const profileBio = document.createElement('p');
+            profileBio.textContent = 'suggested for you';
+            textContainer.appendChild(profileBio);
+
+            userContainer.appendChild(textContainer);
+
+            const followLink = document.createElement('a');
+            followLink.href = '';
+            followLink.textContent = 'Follow';
+            followLink.id = 'follow';
+            userContainer.appendChild(followLink);
+
+            profileContainer.appendChild(userContainer);
+        });
+    });
+};
+
+const showStoriesPanel = token => {
+    fetch("https://photo-app-secured.herokuapp.com/api/stories/", {
+        method: "GET",
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + token
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        const profileContainer = document.querySelector('#story');
+
+        // Clear any existing suggestions in the container
+       profileContainer.innerHTML = '';
+
+        // Loop through each suggested user and create an HTML representation for them
+        data.forEach(user => {
+            console.log(user);
+            const userContainer = document.createElement('div');
+            userContainer.id = 'story-card';
+
+            const profileImage = document.createElement('img');
+            profileImage.src = user.image_url;
+            profileImage.alt = 'Profile Image';
+            userContainer.appendChild(profileImage);
+
+            const textContainer = document.createElement('div');
+            textContainer.id = 'text-story';
+
+
+            profileContainer.appendChild(userContainer);
+        });
     });
 }
 
